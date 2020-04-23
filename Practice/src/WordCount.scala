b4dfe11d248839a0bcd438c51f8e936008450817
tsaum <tsaum@LAPTOP-DS23FN0O>
2020-04-23 07:34:43 +0530

    DF Example added

--- Practice/src/WordCount.scala
+++ Practice/src/WordCount.scala
@@ -12,16 +12,9 @@ object WordCount {
     //Read the input file to a dataframe
 
     val file1 = sc.textFile("textfile1.txt")
-    val file2 = sc.textFile("textfile2.txt")
     val count = file1.flatMap(x => x.split("|")).map(x => (x, 1)).reduceByKey(_ + _)
     
-    count.collect.foreach(println)
-/*    val kf1 = file1.map(x => x.split("|")).map(x => (x(0), x))
-    val kf2 = file2.map(x => x.split("|")).map(x => (x(0), x))
-
-    val word = file1.map(x => x.split("|")).foreach(println)
-*/  
-    
+    count.collect.foreach(println)    
   val abcd=file1.map(x=>x.split("\\|")).map(x=>List(x(0),x(1),x(2)))
   .foreach(println)
   
